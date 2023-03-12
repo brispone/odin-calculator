4289d8bb4dd40351d93107269a78a728a474a330
Bradley Rispone <bradrispone@gmail.com>
2023-03-12 17:31:13 -0500

    Get basic math operations working properly

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const calculator = {
-    currentDisplay: "",
+    currentDisplay: "0",
+    workingValue: "",
     runningTotal: 0,
     operator: ""
 };
+
+function checkZero() {
+    if(calculator.currentDisplay === "0") {
+        return true;
+    } else return false;
+}
+
+function checkForOperator() {
+    if(calculator.currentDisplay === "+" ||
+       calculator.currentDisplay === "-" ||
+       calculator.currentDisplay === "x" ||
+       calculator.currentDisplay === "÷"    ) {
+            return true;
+    } else return false;
+}
 
 function add(x, y) {
     return x + y;
@@ -20,83 +36,96 @@ function divide(x, y) {
     return x / y;
 }
 
-function operate(operator, x, y) {
-    switch(operator) {
+function operate() {
+    let value = 0;
+    let x = parseFloat(calculator.previousValue);
+    let y = parseFloat(calculator.currentDisplay);
+    switch(calculator.operator) {
         case "+":
-            return add(x, y);
+            value = add(x, y);
+            break;
         case "-":
-            return subtract(x, y);
+            value = subtract(x, y);
+            break;
         case "*":
-            return multiply(x, y);
+            value = multiply(x, y);
+            break;
         case "/":
-            return divide(x, y);
+            value = divide(x, y);
     }
+
+    calculator.runningTotal = value;
+    calculator.currentDisplay = calculator.runningTotal;
+    updateDisplay();
 }
 
 // Button Event Listeners
 
 function updateDisplay() {
     const display = document.querySelector("#calc-display");
-    if(calculator.currentDisplay === "") {
-        display.innerText = "0";
-    } else display.innerText = calculator.currentDisplay;
+    display.innerText = calculator.currentDisplay;
 }
+
 // NUMBER BUTTONS
 
 document.querySelector("#one-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "1";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "1" : calculator.currentDisplay += "1";
     updateDisplay();
 });
 
 document.querySelector("#two-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "2";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "2" : calculator.currentDisplay += "2";
     updateDisplay();
 });
 
 document.querySelector("#three-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "3";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "3" : calculator.currentDisplay += "3";
     updateDisplay();
 });
 
 document.querySelector("#four-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "4";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "4" : calculator.currentDisplay += "4";
     updateDisplay();
 });
 
 document.querySelector("#five-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "5";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "5" : calculator.currentDisplay += "5";
     updateDisplay();
 });
 
 document.querySelector("#six-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "6";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "6" : calculator.currentDisplay += "6";
     updateDisplay();
 });
 
 document.querySelector("#seven-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "7";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "7" : calculator.currentDisplay += "7";
     updateDisplay();
 });
 
 document.querySelector("#eight-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "8";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "8" : calculator.currentDisplay += "8";
     updateDisplay();
 });
 
 document.querySelector("#nine-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "9";
+    (checkZero() || checkForOperator()) ? calculator.currentDisplay = "9" : calculator.currentDisplay += "9";
     updateDisplay();
 });
 
 document.querySelector("#zero-button").addEventListener("click", ()=> {
-    calculator.currentDisplay += "0";
+    if(calculator.currentDisplay === "0") {
+        return;
+    } else {
+        checkForOperator() ? calculator.currentDisplay = "0" : calculator.currentDisplay += "0";
     updateDisplay();
+    }
 }); 
 
 // CLEAR BUTTON
 
 document.querySelector("#clear-button").addEventListener("click", ()=> {
-    calculator.currentDisplay = "";
+    calculator.currentDisplay = "0";
     calculator.runningTotal = 0;
     updateDisplay();
 });
@@ -104,25 +133,34 @@ document.querySelector("#clear-button").addEventListener("click", ()=> {
 // OPERATOR BUTTONS
 
 document.querySelector("#add-button").addEventListener("click", ()=> {
+    calculator.previousValue = calculator.currentDisplay;
     calculator.currentDisplay = "+";
     calculator.operator = "+";
     updateDisplay();
 });
 
 document.querySelector("#subtract-button").addEventListener("click", ()=> {
+    calculator.previousValue = calculator.currentDisplay;
     calculator.currentDisplay = "-";
     calculator.operator = "-";
     updateDisplay();
 }); 
 
 document.querySelector("#multiply-button").addEventListener("click", ()=> {
-    calculator.currentDisplay = "-";
-    calculator.operator = "-";
+    calculator.previousValue = calculator.currentDisplay;
+    calculator.currentDisplay = "x";
+    calculator.operator = "*";
     updateDisplay();
 }); 
 
 document.querySelector("#divide-button").addEventListener("click", ()=> {
-    calculator.currentDisplay = "-";
-    calculator.operator = "-";
+    calculator.previousValue = calculator.currentDisplay;
+    calculator.currentDisplay = "÷";
+    calculator.operator = "/";
+    updateDisplay();
+}); 
+
+document.querySelector("#equals-button").addEventListener("click", ()=> {
+    operate();
     updateDisplay();
 }); 
